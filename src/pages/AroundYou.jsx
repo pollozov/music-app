3e0c7068479ef9c674e381cbb36a508aa498dcc9
Polina Loz <pollozov@gmail.com>
2023-02-10 12:46:43 +0100

    done with TopCharts

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -12,8 +12,6 @@ const AroundYou = () => {
     const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
-    console.log('data1', data);
-
     useEffect(() => {
         axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_NbK2yMxnzxY0glJPWuY4agWlVYnTE')
             .then((res) => setCountry(res?.data?.location?.country))
@@ -27,7 +25,9 @@ const AroundYou = () => {
  
     return(
         <div className="flex flex-col">
-            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around you</h2>
+            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+                Around you <span className="font-black">{country}</span>
+            </h2>
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {data?.tracks.map((song, i) => (
                     <SongCard 
